784c8ff8393abfd1e60c3c090f60c2cb7e087fcd
rockyb <rockyb>
2008-03-16 21:14:13 +0000

    *** empty log message ***

--- trunk/lib/columnize.rb
+++ trunk/lib/columnize.rb
@@ -1,6 +1,6 @@
 # $Id$
 # 
-#   Copyright (C) 2007 Rocky Bernstein <rockyb@rubyforge.net>
+#   Copyright (C) 2007, 2008 Rocky Bernstein <rockyb@rubyforge.net>
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
