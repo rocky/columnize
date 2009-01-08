d314d4ec644b33de1b553e7926c301d626e1e231
rockyb <rockyb>
2009-01-08 09:27:51 +0000

    Fix bad bug in arranging horizontally

--- trunk/lib/columnize.rb
+++ trunk/lib/columnize.rb
@@ -1,6 +1,6 @@
 # $Id$
 # 
-#   Copyright (C) 2007, 2008 Rocky Bernstein <rockyb@rubyforge.net>
+#   Copyright (C) 2007, 2008, 2009 Rocky Bernstein <rockyb@rubyforge.net>
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -37,15 +37,26 @@
 
 module Columnize
 
-  # Return a string with embedded newlines (\n) arranging +list+ in
-  # column-order so that each line is no larger than +displaywidth+.
-  # If +list+ is not an array, the empty string, '', is returned.
-  # +colsep+ contains the string to use to separate entries. Normally,
-  # consecutive items go down from the top to bottom from the
-  # left-most column to the right-most. If +arrange_vertical+ is set
-  # false, consecutive items will go across, left to right, top to
-  # bottom.
-  def columnize(list, displaywidth=80, colsep = '  ', arrange_vertical=true)
+  # Return a list of strings with embeeded newlines (\n) as a compact
+  # set of columns arranged horizontally or vertically.
+  #
+  # For example, for a line width of 4 characters (arranged vertically):
+  #     ['1', '2,', '3', '4'] => '1  3\n2  4\n'
+   
+  # or arranged horizontally:
+  #     ['1', '2,', '3', '4'] => '1  2\n3  4\n'
+  #     
+  # Each column is only as wide possible, no larger than
+  # +displaywidth'.  If +list+ is not an array, the empty string, '',
+  # is returned. By default, columns are separated by two spaces - one
+  # was not legible enough. Set +colsep+ to adjust the string separate
+  # columns. If +arrange_vertical+ is set false, consecutive items
+  # will go across, left to right, top to bottom.
+
+  def columnize(list, displaywidth=80, colsep = '  ', 
+                arrange_vertical=true, ljust=true)
+
+    # Some degenerate cases
     if not list.is_a?(Array)
       return ''
     end
@@ -56,83 +67,154 @@ module Columnize
     if 1 == l.size
       return "#{l[0]}\n"
     end
-    # Consider arranging list in 1 rows total, then 2 rows...
-    # Stop when at the smallest number of rows which
-    # can be arranged less than the display width.
-    nrows = 0; ncols = l.size
-    
+
+    nrows = ncols = 0  # Make nrows, ncols have more global scope
+    colwidths = []     # Same for colwidths
     if arrange_vertical
       array_index = lambda {|nrows, row, col| nrows*col + row }
-    else
-      array_index = lambda {|nrows, row, col| nrows*row + col }
-    end
+      # Try every row count from 1 upwards
+      1.upto(l.size-1) do |nrows|
+        ncols = (l.size + nrows-1) / nrows
+        colwidths = []
+        totwidth = -colsep.length
 
-    colwidths = []
-    1.upto(l.size) do 
-      colwidths = []
-      nrows += 1
-      totwidth = -colsep.length
-      col = 0
-      0.upto(ncols-1) do |col|
-        # get max column width for this column
-        colwidth = 0
-        0.upto(nrows-1) do |row|
-          i = array_index.call(nrows, row, col)
-          if i >= l.size
+        0.upto(ncols-1) do |col|
+          # get max column width for this column
+          colwidth = 0
+          0.upto(nrows-1) do |row|
+            i = array_index.call(nrows, row, col)
+            if i >= l.size
+              break
+            end
+            colwidth = [colwidth, l[i].size].max
+          end
+          colwidths << colwidth
+          totwidth += colwidth + colsep.length
+          if totwidth > displaywidth
+            ncols = col
             break
           end
-          colwidth = [colwidth, l[i].size].max
         end
-        colwidths << colwidth
-        totwidth += colwidth + colsep.length
-        if totwidth > displaywidth
-          ncols = col
+        if totwidth <= displaywidth
           break
         end
       end
-      if totwidth <= displaywidth
-        break
+      # The smallest number of rows computed and the
+      # max widths for each column has been obtained.
+      # Now we just have to format each of the
+      # rows.
+      s = ''
+      0.upto(nrows-1) do |row| 
+        texts = []
+        0.upto(ncols-1) do |col|
+          i = array_index.call(nrows, row, col)
+          if i >= l.size
+            x = ""
+          else
+            x = l[i]
+          end
+          texts << x
+        end
+        while texts and texts[-1] == ''
+          texts = texts[0..-2]
+        end
+        if texts.size > 0
+          0.upto(texts.size-1) do |col|
+            if ljust
+                texts[col] = texts[col].ljust(colwidths[col])
+            else
+                texts[col] = texts[col].rjust(colwidths[col])
+            end
+          end
+          s += "%s\n" % texts.join(colsep)
+        end
       end
-    end
-    # The smallest number of rows computed and the
-    # max widths for each column has been obtained.
-    # Now we just have to format each of the
-    # rows.
-    s = ''
-    0.upto(nrows-1) do |row| 
-      texts = []
-      0.upto(ncols-1) do |col|
-        i = array_index.call(nrows, row, col)
-        if i >= l.size
-          if arrange_vertical:
-              x = ""
-          else 
+      return s
+    else
+      array_index = lambda {|nrows, row, col| ncols*(row-1) + col }
+      # Try every column count from size downwards
+      # Assign to make enlarge scope of loop variables 
+      totwidth = i = rounded_size = 0  
+      l.size.downto(0) do |ncols|
+        # Try every row count from 1 upwards
+        min_rows = (l.size+ncols-1) / ncols
+        min_rows.upto(l.size) do |nrows|
+          rounded_size = nrows * ncols
+          colwidths = []
+          totwidth = -colsep.length
+          colwidth = row = 0
+          0.upto(ncols-1) do |col|
+            # get max column width for this column
+            1.upto(nrows) do |row|
+              i = array_index.call(nrows, row, col)
+              if i >= rounded_size 
+                break
+              elsif i < l.size
+                colwidth = [colwidth, l[i].size].max
+              end
+            end
+            colwidths << colwidth
+            totwidth += colwidth + colsep.length
+            if totwidth > displaywidth
+              break
+            end
+          end
+          if totwidth <= displaywidth
+            # Found the right nrows and ncols
+            nrows  = row
+            break
+          elsif totwidth >= displaywidth
+            # Need to reduce ncols
             break
           end
-        else
-          x = l[i]
         end
-        texts << x
+        if totwidth <= displaywidth and i >= rounded_size-1
+            break
+        end
       end
-      while texts and texts[-1] == ''
-        texts = texts[0..-2]
-      end
-      if texts.size > 0
+      # The smallest number of rows computed and the
+      # max widths for each column has been obtained.
+      # Now we just have to format each of the
+      # rows.
+      s = ''
+      1.upto(nrows) do |row| 
+        texts = []
+        0.upto(ncols-1) do |col|
+          i = array_index.call(nrows, row, col)
+          if i >= l.size
+            break
+          else
+            x = l[i]
+          end
+          texts << x
+        end
         0.upto(texts.size-1) do |col|
-          texts[col] = texts[col].ljust(colwidths[col]) if 
-            colwidths[col]
+          if ljust
+            texts[col] = texts[col].ljust(colwidths[col])
+          else
+            texts[col] = texts[col].rjust(colwidths[col])
+          end
         end
         s += "%s\n" % texts.join(colsep)
       end
+      return s
     end
-    return s
   end
   module_function :columnize
 end
 if __FILE__ == $0
   #
-  puts Columnize::columnize(5)
   include Columnize
+  
+  [[4, 4], [4, 7], [100, 80]].each do |width, num|
+    data = (1..num).map{|i| i.to_s}
+    [[false, 'horizontal'], [true, 'vertical']].each do |bool, dir|
+      puts "Width: #{width}, direction: #{dir}"
+      print columnize(data, width, '  ', arrange_vertical=bool)
+      end
+  end
+
+  puts Columnize::columnize(5)
   puts columnize([])
   puts columnize(["a", 2, "c"], 10, ', ')
   puts columnize(["oneitem"])
