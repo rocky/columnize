03c481286a9045f8cd093f6e17a72c3b772e3cf0
rockyb <rockyb>
2010-09-20 16:27:05 +0000

    Add .gemspec, correct description field and add a summary. Add Columnize::VERSION Simplify Rakefile Add stub Makefiles

---
+++ trunk/Makefile
@@ -0,0 +1,10 @@
+# Whatever it is you want to do, it should be forwarded to the 
+# to top-level irectories
+PHONY=check test all
+
+test: check
+all: check
+	true
+
+%: 
+	rake $@
--- trunk/lib/columnize.rb
+++ trunk/lib/columnize.rb
@@ -1,6 +1,6 @@
 # $Id$
 # 
-#   Copyright (C) 2007, 2008, 2009 Rocky Bernstein <rockyb@rubyforge.net>
+#   Copyright (C) 2007, 2008, 2009, 2010 Rocky Bernstein <rockyb@rubyforge.net>
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
