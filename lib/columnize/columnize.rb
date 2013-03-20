c276adb82c263d50cd484bd6486669fa3135e5ca
waslogic <waslogic@gmail.com>
2013-03-20 08:23:07 -0400

    now using the passed in opts hash as an interface for the class which allows a single instance to more easily be reused; next up, freezing the @opts hash to make sure that the interface is obeyed rather than abused

--- lib/columnize/columnize.rb
+++ lib/columnize/columnize.rb
@@ -6,6 +6,8 @@ module Columnize
   class Columnizer
     ARRANGE_ARRAY_OPTS = {:array_prefix => '[', :line_prefix => ' ', :line_suffix => ",\n", :array_suffix => "]\n", :colsep => ', ', :arrange_vertical => false}
     OLD_AND_NEW_KEYS = {:lineprefix => :line_prefix, :linesuffix => :line_suffix}
+    # TODO: change colfmt to cell_format; change colsep to something else
+    ATTRS = [:arrange_vertical, :array_prefix, :array_suffix, :line_prefix, :line_suffix, :colfmt, :colsep, :displaywidth, :ljust]
 
     attr_reader :list, :opts
 
@@ -24,13 +26,12 @@ module Columnize
       end
     end
 
+    # TODO: freeze @opts
     def opts=(opts)
       @opts = opts
       OLD_AND_NEW_KEYS.each {|old, new| @opts[new] = @opts.delete(old) if @opts.keys.include?(old) and !@opts.keys.include?(new) }
       @opts.merge!(ARRANGE_ARRAY_OPTS) if @opts[:arrange_array]
-      @opts[:ljust] = !@list.all? {|datum| datum.kind_of?(Numeric)} if @opts[:ljust] == :auto
-      adjust_displaywidth
-      @stringify = @opts[:colfmt] ? lambda {|li| @opts[:colfmt] % li } : lambda {|li| li.to_s }
+      set_attrs_from_opts
     end
 
     def update_opts(opts)
@@ -42,14 +43,14 @@ module Columnize
 
       rows, colwidths = compute_rows_and_colwidths
       ncols = colwidths.length
-      justify = lambda {|t, c| @opts[:ljust] ? t.ljust(colwidths[c]) : t.rjust(colwidths[c]) }
+      justify = lambda {|t, c| @ljust ? t.ljust(colwidths[c]) : t.rjust(colwidths[c]) }
       textify = lambda do |s, row|
-        row.map!.with_index(&justify) unless ncols == 1 && @opts[:ljust]
-        s + "#{@opts[:line_prefix]}#{row.join(@opts[:colsep])}#{@opts[:line_suffix]}"
+        row.map!.with_index(&justify) unless ncols == 1 && @ljust
+        s + "#{@line_prefix}#{row.join(@colsep)}#{@line_suffix}"
       end
 
       text = rows.inject('', &textify)
-      text = text.sub(@opts[:line_prefix], @opts[:array_prefix]) + @opts[:array_suffix] unless @opts[:array_prefix].empty?
+      text = text.sub(@line_prefix, @array_prefix) + @array_suffix unless @array_prefix.empty?
       text
     end
 
@@ -57,19 +58,19 @@ module Columnize
     # compute the smallest number of rows and the max widths for each column
     def compute_rows_and_colwidths
       list = @list.map &@stringify
-      cell_widths = list.map {|x| cell_size x }
-      # default is 1 atom per row (just in case any atom > @opts[:displaywidth])
+      cell_widths = list.map(&@term_adjuster).map(&:size)
+      # default is 1 atom per row (just in case any atom > @displaywidth)
       rcw = [rows_and_cols(list, 1)[0], [cell_widths.max]]
-      return rcw if rcw[1][0] > @opts[:displaywidth]
+      return rcw if rcw[1][0] > @displaywidth
 
       # TODO: explain why
       sizes, ri, ci = (1..list.length).to_a, 1, 0
-      sizes, ri, ci = sizes.reverse, 0, 1 unless @opts[:arrange_vertical]
+      sizes, ri, ci = sizes.reverse, 0, 1 unless @arrange_vertical
 
       sizes.each do |size|
         colwidths = rows_and_cols(cell_widths, size)[ci].map(&:max)
-        totwidth = colwidths.inject(&:+) + ((colwidths.length-1) * @opts[:colsep].length)
-        rcw = [rows_and_cols(list, size)[ri], colwidths] and break if totwidth <= @opts[:displaywidth]
+        totwidth = colwidths.inject(&:+) + ((colwidths.length-1) * @colsep.length)
+        rcw = [rows_and_cols(list, size)[ri], colwidths] and break if totwidth <= @displaywidth
       end
       rcw
     end
@@ -83,25 +84,15 @@ module Columnize
       [rows, cols]
     end
 
-    # Return the length of String +cell+. If Boolean +term_adjust+ is true, ignore terminal sequences in +cell+.
-    def cell_size(cell)
-      if @opts[:term_adjust]
-        cell.gsub(/\e\[.*?m/, '')
-      else
-        cell
-      end.size
-    end
-
-    def adjust_displaywidth
-      if @opts[:displaywidth] - @opts[:line_prefix].length < 4
-        @opts[:displaywidth] = @opts[:line_prefix].length + 4
-      else
-        @opts[:displaywidth] -= @opts[:line_prefix].length
-      end
-    end
-
     def set_attrs_from_opts
       # TODO: START HERE: make everything that can be an attr, be an attr
+      ATTRS.each {|attr| self.instance_variable_set "@#{attr}", @opts[attr] }
+
+      @ljust = !@list.all? {|datum| datum.kind_of?(Numeric)} if @ljust == :auto
+      @displaywidth -= @line_prefix.length
+      @displaywidth = @line_prefix.length + 4 if @displaywidth < 4
+      @stringify = @colfmt ? lambda {|li| @colfmt % li } : lambda {|li| li.to_s }
+      @term_adjuster = @opts[:term_adjust] ? lambda {|c| c.gsub(/\e\[.*?m/, '') } : lambda {|c| c }
     end
   end
 end
--- test/test-columnizer.rb
+++ test/test-columnizer.rb
@@ -6,6 +6,9 @@ class TestColumnizer < Test::Unit::TestCase
   TOP_SRC_DIR = File.join(File.expand_path(File.dirname(__FILE__)), '..', 'lib')
   require File.join(TOP_SRC_DIR, 'columnize.rb')
 
+  Columnize::Columnizer.class_eval 'attr_reader :stringify, :short_circuit, :term_adjuster'
+  Columnize::Columnizer.class_eval 'attr_reader *ATTRS'
+
   # SETTING OPTS IN INITIALIZE
   def test_passed_in_opts
     # passed in opts should be merged with DEFAULT_OPTS
@@ -14,34 +17,48 @@ class TestColumnizer < Test::Unit::TestCase
     assert_equal 15, c.opts[:displaywidth], 'displaywidth should override DEFAULT_OPTS'
   end
 
-  def test_ljust_opt
+  def test_ljust_attr
     c = Columnize::Columnizer.new([1,2,3], {:ljust => :auto})
-    assert_equal false, c.opts[:ljust], 'ljust: :auto should transform to false when all values are numeric'
+    assert_equal false, c.ljust, 'ljust: :auto should transform to false when all values are numeric'
     c = Columnize::Columnizer.new(['1', 2, 3], {:ljust => :auto})
-    assert_equal true, c.opts[:ljust], 'ljust: :auto should transform to true when not all values are numeric'
+    assert_equal true, c.ljust, 'ljust: :auto should transform to true when not all values are numeric'
     c = Columnize::Columnizer.new([], {:ljust => false})
-    assert_equal false, c.opts[:ljust], 'ljust: false should stay false'
+    assert_equal false, c.ljust, 'ljust: false should stay false'
     c = Columnize::Columnizer.new([], {:ljust => true})
-    assert_equal true, c.opts[:ljust], 'ljust: true should stay true'
+    assert_equal true, c.ljust, 'ljust: true should stay true'
   end
 
   def test_stringify_attr
-    stringify = Columnize::Columnizer.new.instance_variable_get(:@stringify)
-    assert_equal '1', stringify[1], 'without colfmt, should be to_s'
-    stringify = Columnize::Columnizer.new([], :colfmt => '%02d').instance_variable_get(:@stringify)
-    assert_equal '01', stringify[1], 'without colfmt, should be to_s'
+    c = Columnize::Columnizer.new
+    assert_equal '1.0', c.stringify[1.0], 'without colfmt, should be to_s'
+    c.update_opts :colfmt => '%02d'
+    assert_equal '01', c.stringify[1.0], 'without colfmt, should be to_s'
   end
 
   def test_short_circuit_attr
     c = Columnize::Columnizer.new
-    assert_equal "<empty>\n", c.instance_variable_get(:@short_circuit), 'should explicitly state when empty'
+    assert_equal "<empty>\n", c.short_circuit, 'should explicitly state when empty'
     c.list = 1
-    assert_equal '', c.instance_variable_get(:@short_circuit), 'should be an empty string when not an array'
+    assert_equal '', c.short_circuit, 'should be an empty string when not an array'
     c.list = [1]
-    assert_equal nil, c.instance_variable_get(:@short_circuit), 'should be nil when list is good'
+    assert_equal nil, c.short_circuit, 'should be nil when list is good'
   end
 
-  # TODO: test list= and opts=
+  def test_term_adjuster_attr
+    c = Columnize::Columnizer.new
+    assert_equal 'abc', c.term_adjuster['abc']
+    assert_equal "\e[0;31mObject\e[0;4m", c.term_adjuster["\e[0;31mObject\e[0;4m"]
+    c.update_opts :term_adjust => true
+    assert_equal 'abc', c.term_adjuster['abc']
+    assert_equal 'Object', c.term_adjuster["\e[0;31mObject\e[0;4m"]
+  end
+
+  def test_displaywidth_attr
+    c = Columnize::Columnizer.new [], :displaywidth => 10, :line_prefix => '        '
+    assert_equal 12, c.displaywidth, 'displaywidth within 4 of line_prefix.length'
+    c.update_opts :line_prefix => '  '
+    assert_equal 8, c.displaywidth, 'displaywidth not within 4 of line_prefix.length'
+  end
 
   # COLUMNIZE
   def test_columnize_with_short_circuit
@@ -53,9 +70,9 @@ class TestColumnizer < Test::Unit::TestCase
 
   def test_columnize_applies_ljust
     c = Columnize::Columnizer.new [1,2,3,10,20,30], :displaywidth => 10, :ljust => false, :arrange_vertical => false
-    assert_equal " 1   2   3\n10  20  30\n", c.columnize, "ljust: #{c.opts[:ljust]}"
-    c.opts[:ljust] = true
-    assert_equal "1   2   3 \n10  20  30\n", c.columnize, "ljust: #{c.opts[:ljust]}"
+    assert_equal " 1   2   3\n10  20  30\n", c.columnize, "ljust: #{c.ljust}"
+    c.update_opts :ljust => true
+    assert_equal "1   2   3 \n10  20  30\n", c.columnize, "ljust: #{c.ljust}"
   end
 
   def test_columnize_applies_colsep_and_prefix_and_suffix
@@ -79,22 +96,6 @@ class TestColumnizer < Test::Unit::TestCase
     rows,cols = Columnize::Columnizer.new.rows_and_cols((1..9).to_a, 3)
     assert_equal [[1,2,3],[4,5,6],[7,8,9]], rows, 'rows'
     assert_equal [[1,4,7],[2,5,8],[3,6,9]], cols, 'cols'
-  end
-
-  def test_cell_size
-    c = Columnize::Columnizer.new
-    assert_equal(3, c.cell_size('abc'))
-    assert_equal(19, c.cell_size("\e[0;31mObject\e[0;4m"))
-    c.opts[:term_adjust] = true
-    assert_equal(3, c.cell_size('abc'))
-    assert_equal(6, c.cell_size("\e[0;31mObject\e[0;4m"))
-  end
-
-  def test_adjust_display_width
-    c = Columnize::Columnizer.new [], :displaywidth => 10, :line_prefix => '        '
-    assert_equal 12, c.opts[:displaywidth], 'displaywidth within 4 of line_prefix.length'
-    c.update_opts :displaywidth => 10, :line_prefix => '  '
-    assert_equal 8, c.opts[:displaywidth], 'displaywidth not within 4 of line_prefix.length'
   end
 
   def test_set_attrs_from_opts
